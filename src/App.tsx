483ded7dd116781a01bf1c691224194981643d91
Le Trong Nhan <nhanlt03082000@gmail.com>
2024-07-03 09:32:41 +0700

    feat: Add lazy loading for images in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Fragment } from 'react/jsx-runtime'
 import './App.css'
 import useRouteElements from './useRouteElements'
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   const routeElements = useRouteElements()
 
-  return <>{routeElements}</>
+  return (
+    <Fragment>
+      <ToastContainer />
+      {routeElements}
+    </Fragment>
+  )
 }
 
 export default App
