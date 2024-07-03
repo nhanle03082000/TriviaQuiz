08b14dfafac7b7a5f82fb518020a2a5ee36b15e3
Le Trong Nhan <nhanlt03082000@gmail.com>
2024-07-03 15:29:23 +0700

    refactor: Remove unused QuizSlice import and update Redux store configuration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { quizReducer, QuizState } from './slice/QuizSlice'
 import { appReducer, AppState } from './slice/AppSlice'
+import { QuizState } from './slice/QuizSlice'
 
 export const store = configureStore({
   reducer: {
-    quiz: quizReducer,
     app: appReducer
   }
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-export type { QuizState }
-export type { AppState }
+export type { AppState, QuizState }
 
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch)
