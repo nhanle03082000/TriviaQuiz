66833231af4d6582c05da5bb111c8efd4b1b61e4
Le Trong Nhan <nhanlt03082000@gmail.com>
2024-07-03 08:30:13 +0700

    feat: Implement lazy loading for routes in useRouteElement hook

--- src/useRouteElement.tsx
+++ src/useRouteElement.tsx
@@ -1,6 +1,25 @@
+import React from 'react'
 import { useRoutes } from 'react-router-dom'
 
+// TODO: Add lazy loading for the routes
+const TriviaSetup = React.lazy(() => import('./pages/TriviaSetup'))
+const Quiz = React.lazy(() => import('./pages/Quiz'))
+const Results = React.lazy(() => import('./pages/Results'))
+
 export default function useRouteElement() {
-  const routeElement = useRoutes([])
+  const routeElement = useRoutes([
+    {
+      path: '/',
+      element: <TriviaSetup />
+    },
+    {
+      path: '/quiz',
+      element: <Quiz />
+    },
+    {
+      path: '/results',
+      element: <Results />
+    }
+  ])
   return routeElement
 }
