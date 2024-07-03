a87dd06233c1612bc2331660b28465676e66fe6b
Le Trong Nhan <nhanlt03082000@gmail.com>
2024-07-03 11:23:49 +0700

    feat: Dispatch selected category and difficulty to store in TriviaSetup component

--- src/pages/TriviaSetup/TriviaSetup.tsx
+++ src/pages/TriviaSetup/TriviaSetup.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { toast } from 'react-toastify'
 import categoryApi from '~/apis/triviaQuiz.api'
 import { difficultySelect } from '~/constants/difficultySelect'
 import HttpStatusCode from '~/constants/httpStatusCode.enum'
+import { handleSubmitQuiz } from '~/redux/slice/QuizSlice'
 import { ItemCategory } from '~/types/category.type'
 import { isAxiosError } from '~/utils/utils'
 
 const TriviaSetup = () => {
+  const dispatch = useDispatch()
   const [categories, setCategories] = useState<ItemCategory[]>([])
   const [selectedCategory, setSelectedCategory] = useState('')
   const [selectedDifficulty, setSelectedDifficulty] = useState('easy')
@@ -16,11 +19,13 @@ const TriviaSetup = () => {
   }, [])
 
   const getCategory = async () => {
+    //TODO: Get categories from the API
     try {
       const response = await categoryApi.getCategories()
       if (response.status !== HttpStatusCode.Ok) {
         throw new Error('Failed to fetch categories')
       }
+      // Set the categories state
       setCategories(response.data.trivia_categories)
     } catch (error) {
       if (isAxiosError(error)) {
@@ -39,7 +44,10 @@ const TriviaSetup = () => {
     setSelectedDifficulty(e.target.value)
   }
 
-  const startQuiz = () => {}
+  const startQuiz = () => {
+    // TODO: Dispatch the selected category and difficulty to the store using the handleSubmitQuiz action
+    dispatch(handleSubmitQuiz({ category: selectedCategory, difficulty: selectedDifficulty }))
+  }
   return (
     <div>
       <select id='categorySelect' value={selectedCategory} onChange={handleChangeCategory} aria-label='Select Category'>
