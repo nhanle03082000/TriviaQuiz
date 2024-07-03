7ff5e7cd965dc6005eddb7a8b3697235cf092be8
Le Trong Nhan <nhanlt03082000@gmail.com>
2024-07-03 11:17:00 +0700

    feat: Add AppSlice for managing app loading state

---
+++ src/redux/slice/AppSlice.ts
@@ -0,0 +1,38 @@
+import { createSelector, createSlice } from '@reduxjs/toolkit'
+
+interface AppState {
+  loading: boolean
+}
+
+const sliceName = 'app'
+const initialState: AppState = {
+  loading: false
+}
+
+const appSlice = createSlice({
+  name: sliceName,
+  initialState,
+  reducers: {
+    toggleLoading(state) {
+      state.loading = !state.loading
+    },
+    startLoading(state) {
+      state.loading = true
+    },
+    stopLoading(state) {
+      state.loading = false
+    }
+  }
+})
+
+// ===========================================================
+// >>>> FOR EXPORTS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// ===========================================================
+const appState = (state: { [sliceName]: AppState }) => state[sliceName]
+
+export const appSelector = {
+  loading: createSelector(appState, (app) => app.loading)
+}
+
+export const appActions = appSlice.actions
+export const appReducer = appSlice.reducer
