147d90da0824625248b48e5173aa00b8c62e71ca
Le Trong Nhan <nhanlt03082000@gmail.com>
2024-07-04 09:41:12 +0700

    refactor: Shuffle quiz answers in ListQuiz component

--- src/pages/ListQuiz/ListQuiz.tsx
+++ src/pages/ListQuiz/ListQuiz.tsx
@@ -10,7 +10,7 @@ import { SelectedAnswers } from '~/types/selectedAnswers.type'
 import { isAxiosError } from '~/utils/utils'
 import QuizFilter from './components/QuizFilter'
 import path from '~/constants/path'
-
+import _ from 'lodash'
 const ListQuiz = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -22,13 +22,19 @@ const ListQuiz = () => {
       try {
         // Start loading
         dispatch(appActions.startLoading())
-        const response = await questionsApi.getCategories(category, difficulty)
+        const response = await questionsApi.fetchQuizData(category, difficulty)
         // Check if the response status is not OK
         if (response.status !== HttpStatusCode.Ok) {
           throw new Error('Failed to fetch categories')
         }
-        // Set the listQuiz state
-        setListQuiz(response.data.results)
+        // use lodash to shuffle the answers
+        const shuffledQuestions = response.data.results.map((question) => {
+          return {
+            ...question,
+            answers: _.shuffle([...question.incorrect_answers, question.correct_answer])
+          }
+        })
+        setListQuiz(shuffledQuestions)
       } catch (error) {
         if (isAxiosError(error)) {
           toast.error(error.message)
@@ -41,6 +47,7 @@ const ListQuiz = () => {
     },
     [dispatch]
   )
+
   const handleAnswerSelect = useCallback((questionId: string, answer: string) => {
     // TODO: Update the selectedAnswers state
     // If the answer is already selected, keep it, otherwise update it
@@ -56,12 +63,14 @@ const ListQuiz = () => {
       state: { answers: selectedAnswers, questions: listQuiz }
     })
   }, [navigate, selectedAnswers, listQuiz])
+
   const allQuestionsAnswered = useCallback(() => {
     // TODO: Check if all questions have been answered
     // If the listQuiz length is greater than 0 and every question has been answered
     // eslint-disable-next-line no-prototype-builtins
     return listQuiz.length > 0 && listQuiz.every((quiz) => selectedAnswers.hasOwnProperty(quiz.question))
   }, [listQuiz, selectedAnswers])
+
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>QUIZ MAKER</h1>
@@ -71,7 +80,7 @@ const ListQuiz = () => {
         {listQuiz.map((quiz, index) => (
           <div key={index} className='p-4 bg-white rounded-lg shadow-md'>
             <h2 className='text-xl font-bold text-gray-800'>{quiz.question}</h2>
-            {[...quiz.incorrect_answers, quiz.correct_answer].map((answer, i) => (
+            {quiz.answers.map((answer, i) => (
               <button
                 key={i}
                 className={`p-2 m-2 ${selectedAnswers[quiz.question] === answer ? 'bg-blue-200' : 'bg-gray-200'}`}
--- src/types/questions.type.ts
+++ src/types/questions.type.ts
@@ -10,4 +10,5 @@ export interface Question {
   question: string
   correct_answer: string
   incorrect_answers: string[]
+  answers: string[]
 }
