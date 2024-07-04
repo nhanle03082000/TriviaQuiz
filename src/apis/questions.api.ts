ac97c26554780484e088f8e2270fab76a834fdf8
Le Trong Nhan <nhanlt03082000@gmail.com>
2024-07-04 09:41:03 +0700

    refactor: Rename getCategories to fetchQuizData in questions.api.ts

--- src/apis/questions.api.ts
+++ src/apis/questions.api.ts
@@ -2,7 +2,7 @@ import { Questions } from '~/types/questions.type'
 import http from '~/utils/http'
 const URL_QUESTIONS = '/api.php?amount=5'
 const questionsApi = {
-  getCategories(category: string, difficulty: string) {
+  fetchQuizData(category: string, difficulty: string) {
     return http.get<Questions>(`${URL_QUESTIONS}&category=${category}&difficulty=${difficulty}&type=multiple`)
   }
 }
